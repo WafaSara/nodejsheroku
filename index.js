47844f9bce887ee0a9d6bd9af52c1a49a9173d42
wafa sara <wafaeben775@gmail.com>
2017-04-06 11:33:56 +0200

    add server + packages

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
